9b88badd022a729f93372af4118bac2444dffc0d
om <drompatel5@gmail.com>
2021-10-20 17:17:29 -0400

    reverted and striped old ServoEX class

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,25 @@
 #include <servoEx.cpp>
 #include <range.cpp>
 
-ServoEx s;
+int lPin = 13;
+int lTime = 100;
 
-int pin = 10;
-float maxSpeed = 600;
+void initLed(int pin = lPin){
+    pinMode(pin, OUTPUT);
+}
 
-Range r1 = Range(1000, 1500, 2000);
-Range r2 = Range(-90, 0, 90);
-Range2 r = Range2(r1, r2);
-
-void flashLed(int ms){
-    digitalWrite(13, HIGH);
+void flashLed(int ms = lTime, int pin = lPin, bool delayAfter = true){
+    digitalWrite(pin, HIGH);
     delay(ms);
-    digitalWrite(13, LOW);
+    digitalWrite(pin, LOW);
+    if(delayAfter)
+        delay(ms);
 }
 
 void setup(){
-    Serial.begin(9600);
-    s = ServoEx(pin, maxSpeed, r);
-    //pinMode(13, OUTPUT);
+    initLed();
 }
 
 void loop(){
-    
+    flashLed();
 }
--- src/servoEx.cpp
+++ src/servoEx.cpp
@@ -11,5 +11,53 @@ enum ServoRunMode
 
 class ServoEx
 {
-    
-}; //end of class
+    Servo servo;
+    ServoRunMode runMode;
+    Range2 ranges;
+
+    float targetPos;
+    float targetServoPos;
+    float lastPos;
+
+    long servoSetTime;
+    long commandSetTime;
+
+    float maxSpeed;
+    float speed;
+
+    private:
+        float getRate(ServoRunMode runMode, float currentPos, float targetPos, float val = 0){
+            float dis = getDistance(currentPos, targetPos);
+            
+            if(runMode == MOVE_TO_POS){
+                val = maxSpeed;
+            }
+            else if(runMode == MOVE_TO_POS_WITH_TIME){
+                return dis / val;
+            }
+            if(dis < 0)
+                return -val;
+            return val;
+        }
+
+        float getDistance(float currentPos, float targetPos){
+            return targetPos - currentPos;
+        }
+
+    public:
+        ServoEx(int pin, float maxSpeed, Range2 ranges){
+            servo.attach(pin);
+            this -> maxSpeed = maxSpeed;
+            this -> ranges = ranges;
+        }
+
+        ServoEx(Servo servo, float maxSpeed, Range2 ranges){
+            this -> servo = servo;
+            this -> maxSpeed = maxSpeed;
+            this -> ranges = ranges;
+        }
+
+        bool setCommand(ServoRunMode runMode, float position, float val = 0){
+            
+        }
+}; //end of clas
